00b6551d842f4ba235a322244df78d6e8ec0467d
thiyangt <thiyanga.talagala@monash.edu>
2019-10-30 16:28:12 +1030

    add vote matrix information for yearly data

--- src/main.R
+++ src/main.R
@@ -1,4 +1,55 @@
 ## ---- packages
 library(Mcomp)
 
-## ---- votematrixinfo
+## ---- yearlyoob
+load("data/yearly/yearly_training.rda") # random forest training set 
+load("data/yearly/train_votes.rda") # oob votes from the random forest (see: yearly_cluster_results for more info)
+load("data/yearly/train_predictions_oob.rda") # based on oob prediction (see: yearly_cluster_results for more info)
+votes_oob <- data.frame(train_votes)
+names(votes_oob) <- names(table(train_predictions_oob))
+votes_oob$predicted <- train_predictions_oob
+votes_oob$classlabel <- yearly_training$classlabels
+votes_oob <- votes_oob %>%
+  mutate(id = seq_len(n())) %>%
+  melt(id.var = c("classlabel", "id", "predicted"), na.rm = T) %>%
+  select(-id)
+votes_oob <- votes_oob %>%
+  mutate(classlabel = recode(classlabel, nn="nn",
+                             theta = "theta", wn = "wn", "ARMA/AR/MA" = "ARMA", ARIMA = "ARIMA", "ETS-notrendnoseasonal" = "ETS_NTNS",
+                             "ETS-dampedtrend" = "ETS_DT", "ETS-trend" = "ETS_T", "rwd" = "rwd", "rw" = "rw" ))
+
+votes_oob <- votes_oob %>%
+  mutate(predicted = recode(predicted, nn="nn", theta = "theta",
+                            wn = "wn", "ARMA/AR/MA" = "ARMA", ARIMA = "ARIMA",
+                            "ETS-notrendnoseasonal" = "ETS_NTNS", "ETS-dampedtrend" = "ETS_DT",
+                            "ETS-trend" = "ETS_T","rwd" = "rwd", "rw" = "rw" ))
+
+votes_oob <- votes_oob %>%
+  mutate(variable = recode(variable, nn="nn", theta = "theta", wn = "wn", "ARMA/AR/MA" = "ARMA",
+                           ARIMA = "ARIMA", "ETS-notrendnoseasonal" = "ETS_NTNS", "ETS-dampedtrend" = "ETS_DT",
+                           "ETS-trend" = "ETS_T", "rwd" = "rwd", "rw" = "rw" ))
+# arrange labels
+votes_oob$variable <- factor(votes_oob$variable,
+                             levels = rev(c(
+                               "nn",
+                               "theta",
+                               "wn",
+                               "ARMA",
+                               "ARIMA",
+                               "ETS_NTNS",
+                               "ETS_DT",
+                               "ETS_T",
+                               "rwd",
+                               "rw" )))
+
+oob_boxplot_yearly <- ggplot(votes_oob, aes(x = classlabel, y = value, fill = classlabel)) +
+  geom_boxplot(outlier.size = 0.2, outlier.alpha = 0.4) +
+  ylab("Proportion") +
+  xlab("") +
+  theme(legend.position = "none", legend.title = element_blank(), 
+        legend.text.align = 0, text = element_text(size = 25), axis.text.x = element_text(angle = 90),
+        strip.text = element_text(size = 20)) +
+  guides(fill = guide_legend(reverse = TRUE)) +
+  scale_x_discrete(limits = c("nn", "theta", "wn", "ARMA", "ARIMA", "ETS_NTNS", "ETS_DT", "ETS_T", "rwd", "rw" )) +
+  coord_flip() + facet_wrap(. ~ variable, ncol=5)
+oob_boxplot_yearly
