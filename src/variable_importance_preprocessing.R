14d669a9e188d7e1162e7e4cc334351859c3f1f8
thiyangt <thiyanga.talagala@monash.edu>
2019-11-06 18:22:31 +1100

    working on variable importance dataframe

--- src/variable_importance_preprocessing.R
+++ src/variable_importance_preprocessing.R
@@ -61,4 +61,86 @@ top.yearly <- meanrank_viy_classes %>%
   top_n(n = 5, wt = rank)
 
 
+## Monthly data
+load(here("data","monthly", "trainM_importance.rda"))
+load(here("data", "monthly", "sd_pdf_dfM.rda"))
+load(here("data", "monthly", "sd_ice_dfM.rda"))
+## Permutation based
+train_imp_dfM <- data.frame(trainM_importance)
+train_imp_dfM <- tibble::rownames_to_column(train_imp_dfM, "Feature")
+train_imp_dfM <- within(train_imp_dfM, rm("MeanDecreaseAccuracy", "MeanDecreaseGini"))
+permutation_impM <- train_imp_dfM %>% melt(id.vars = "Feature")
+colnames(permutation_impM) <- c("feature", "class", "score")
+## PDP-based
+sd_pdf_dfM <- tibble::rownames_to_column(sd_pdf_dfM, "class")
+pdp_impM <- sd_pdf_dfM %>% melt(id.vars = "class")
+colnames(pdp_impM) <- c("class", "feature", "score")
+## ICE-based
+sd_ice_dfM <- tibble::rownames_to_column(sd_ice_dfM, "class")
+ice_impM <- sd_ice_dfM %>% melt(id.vars = "class")
+colnames(ice_impM) <- c("class", "feature", "score")
+## Combine the data frames
+importancescoreM <- bind_rows(permutation_impM, pdp_impM)
+importancescoreM <- bind_rows(importancescoreM, ice_impM)
+importancescoreM$VI <- rep(c("permutation", "PDP", "ICE"), each = 510)
+## rank permutation, sd_pdp, and sd_ice scores for each class
+rank_vi_monthly_classes <- importancescoreM %>%
+  group_by(VI, class) %>%
+  mutate(rank = min_rank(score))
+## compute mean rank
+meanrank_vim_classes <- rank_vi_monthly_classes %>% 
+  group_by(class, feature) %>% 
+  summarise_at(vars(c(rank)), funs(mean))
+top.monthly <- meanrank_vim_classes %>%
+  group_by(class) %>%
+  top_n(n = 5, wt = rank)
 
+
+## Hourly data
+# All variable scores into one dataframe
+load(here("data", "hourly", "trainH_importance.rda"))
+load(here("data", "hourly", "sd_pdf_dfH.rda"))
+load(here("data", "hourly", "sd_ice_dfH.rda"))
+## Permutation based
+train_imp_dfH <- data.frame(trainH_importance)
+train_imp_dfH <- tibble::rownames_to_column(train_imp_dfH, "Feature")
+train_imp_dfH <- within(train_imp_dfH, rm("MeanDecreaseAccuracy", "MeanDecreaseGini"))
+permutation_impH <- train_imp_dfH %>% melt(id.vars = "Feature")
+#dim(permutation_impD) # 260 3
+colnames(permutation_impH) <- c("feature", "class", "score")
+## PDP-based
+sd_pdf_dfH <- tibble::rownames_to_column(sd_pdf_dfH, "class")
+pdp_imp <- sd_pdf_dfH %>% melt(id.vars = "class")
+colnames(pdp_imp) <- c("class", "feature", "score")
+## ICE-based
+sd_ice_dfH <- tibble::rownames_to_column(sd_ice_dfH, "class")
+ice_imp <- sd_ice_dfH %>% melt(id.vars = "class")
+colnames(ice_imp) <- c("class", "feature", "score")
+## Combine the data frames
+importancescoreH <- bind_rows(permutation_impH, pdp_imp)
+importancescoreH <- bind_rows(importancescoreH, ice_imp)
+importancescoreH$VI <- rep(c("permutation", "PDP", "ICE"), each = 260)
+## rank permutation, sd_pdp, and sd_ice scores for each class
+importancescoreH$class <- factor(importancescoreH$class,
+                                 levels = c("snaive", "rw", "rwd", "mstlarima", "mstlets", "tbats","stlar",
+                                            "theta","nn","wn"),
+                                 labels = c("snaive", "rw", "rwd", "mstlarima", "mstlets", "tbats","stlar",
+                                            "theta","nn","wn"))
+rank_vi_hourly_classes <- importancescoreH %>%
+  group_by(VI, class) %>%
+  mutate(rank = min_rank(score))
+
+## compute mean rank
+meanrank_vih_classes <- rank_vi_hourly_classes %>% 
+  group_by(feature, class) %>% 
+  summarise_at(vars(c(rank)), funs(mean))
+top.hourly <- meanrank_vih_classes %>%
+  group_by(class) %>%
+  top_n(n = 5, wt = rank)
+
+## Match columns in yearly, monthly and hourly dataframes
+yearly.monthly <- merge(top.yearly,top.monthly, by.y  = c("class","feature"), by.x=c("class", "feature"), all.x = TRUE, all.y = TRUE)
+yearly.monthly <- yearly.monthly %>% rename("yearly.rank" = rank.x) 
+yearly.monthly <- yearly.monthly %>% rename("monthly.rank" = rank.y) 
+yearly.monthly.hourly.rank <- merge(yearly.monthly,top.hourly, by.y  = c("class","feature"), by.x=c("class", "feature"), all.x = TRUE, all.y = TRUE)
+yearly.monthly.hourly.rank <- yearly.monthly.hourly.rank %>% rename("hourly.rank" = rank) 
