f17123abf9878e0b1c01dc3b7f457ff803d0608e
thiyangt <thiyanga.talagala@monash.edu>
2020-09-07 06:44:39 +0530

    working on quarterly data

--- src/flexdashboardviz.R
+++ src/flexdashboardviz.R
@@ -6,11 +6,11 @@ library(plotly)
 ## Yearly
 load("~/PhD_journey/fforms/data/HPCfiles/yearlym4_votes.rda")
 load("data/ym4_true_classlabels.rda")
-set.seed(7)
+set.seed(10011)# 1, 10011
 yheatmap <- iheatmapr::main_heatmap(yearlym4_votes, name="Vote probability") %>%
-  add_col_clustering(method="kmeans", k=4) %>%
   add_row_clustering(method="kmeans", k = 5) %>%
-  add_col_labels() %>%
+  add_col_clustering(method="kmeans", k=4) %>%
+  add_col_labels(size=0.05) %>%
   add_row_title("Time series (each row corresponds to a single time series)") %>%
   add_col_title("Class", side="top") 
 yheatmap
@@ -37,3 +37,13 @@ py <- ggplot(yearly_m4_clus,
   ggtitle("Composition of clusters by class labels (best forecast model)")
 
 ggplotly(py)
+
+
+## PCA
+#head(df)
+#df$class <- yearly_m4_clus$cluster 
+
+#plot_ly(x=df$PC1, y=df$PC2, z=df$PC3, type="scatter3d", 
+#        mode="markers", color=df$class, opacity=0.7, size=18,
+#        colors    = ~c("#1b9e77","#d95f02",
+#                       "#7570b3", "#e7298a", "#66a61e"))
--- src/trueclasslabels_m4competitiondata.R
+++ src/trueclasslabels_m4competitiondata.R
@@ -65,3 +65,45 @@ ym4_true_classlabels <- c(yl0, yl1, yl2, yl3, yl4,
 length(ym4_true_classlabels)
 table(ym4_true_classlabels)
 save(ym4_true_classlabels, file="data/ym4_true_classlabels.rda")
+
+
+# Quarterly
+## Load files
+y0 <- get(load("~/PhD_journey/fforms/data/M4_benchmar_Accuracy/yearly/M4yearly_0.rda"))
+y1 <- get(load("~/PhD_journey/fforms/data/M4_benchmar_Accuracy/yearly/M4yearly_1.rda"))
+y2 <- get(load("~/PhD_journey/fforms/data/M4_benchmar_Accuracy/yearly/M4yearly_2.rda") )
+y3 <- get(load("~/PhD_journey/fforms/data/M4_benchmar_Accuracy/yearly/M4yearly_3.rda")) 
+y4 <- get(load("~/PhD_journey/fforms/data/M4_benchmar_Accuracy/yearly/M4yearly_4.rda")) 
+y5 <- get(load("~/PhD_journey/fforms/data/M4_benchmar_Accuracy/yearly/M4yearly_5.rda"))
+y6 <- get(load("~/PhD_journey/fforms/data/M4_benchmar_Accuracy/yearly/M4yearly_6.rda")) 
+y7 <- get(load("~/PhD_journey/fforms/data/M4_benchmar_Accuracy/yearly/M4yearly_7.rda")) 
+y8 <- get(load("~/PhD_journey/fforms/data/M4_benchmar_Accuracy/yearly/M4yearly_8.rda")) 
+y9 <- get(load("~/PhD_journey/fforms/data/M4_benchmar_Accuracy/yearly/M4yearly_9.rda")) 
+
+
+z0 <- cal_medianscaled(y0)
+yl0 <- labels(z0)
+z1 <- cal_medianscaled(y1)
+yl1 <- labels(z1)
+z2 <- cal_medianscaled(y2)
+yl2 <- labels(z2)
+z3 <- cal_medianscaled(y3)
+yl3 <- labels(z3)
+z4 <- cal_medianscaled(y4)
+yl4 <- labels(z4)
+z5 <- cal_medianscaled(y5)
+yl5 <- labels(z5)
+z6 <- cal_medianscaled(y6)
+yl6 <- labels(z6)
+z7 <- cal_medianscaled(y7)
+yl7 <- labels(z7)
+z8 <- cal_medianscaled(y8)
+yl8 <- labels(z8)
+z9 <- cal_medianscaled(y9)
+yl9 <- labels(z9)
+
+ym4_true_classlabels <- c(yl0, yl1, yl2, yl3, yl4,
+                          yl5, yl6, yl7, yl8, yl9)
+length(ym4_true_classlabels)
+table(ym4_true_classlabels)
+save(ym4_true_classlabels, file="data/ym4_true_classlabels.rda")
