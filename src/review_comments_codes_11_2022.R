a4d00ab9f9432a7e02f6396a2b9c7dc15f4155a4
thiyangt <thiyanga.talagala@monash.edu>
2022-11-22 13:35:51 +0530

    completed reviewer comment's calculations

--- src/review_comments_codes_11_2022.R
+++ src/review_comments_codes_11_2022.R
@@ -8,6 +8,7 @@ library(Mcomp)
 library(M4comp2018)
 
 ## ----- length calculations ----
+## M4 competition
 data(M4)
 yearly_M4 <- Filter(function(l) l$period == "Yearly", M4)
 quarterly_M4 <- Filter(function(l) l$period == "Quarterly", M4)
@@ -21,4 +22,49 @@ length_Q <- data.frame(length=sapply(quarterly_M4, function(temp){length(temp$x)
 length_M <- data.frame(length=sapply(monthly_M4, function(temp){length(temp$x)}))
 length_W <- data.frame(length=sapply(weekly_M4, function(temp){length(temp$x)}))
 length_D <- data.frame(length=sapply(daily_M4, function(temp){length(temp$x)}))
-length_H <- sapply(hourly_M4, function(temp){length(temp$x))})
+length_H <- sapply(hourly_M4, function(temp){length(temp$x)})
+
+class(length_Y) # dataframe
+summary(length_Y[,1])
+length(length_Y[, 1])
+
+summary(length_Q[, 1])
+length(length_Q[, 1])
+
+summary(length_M[, 1])
+length(length_M[, 1])
+
+summary(length_D[,1])
+length(length_D[,1])
+
+summary(length_H[, 1])
+length(length_H[, 1])
+## Training data length (M1+M3+Simulated)
+
+## Yearly
+yt <- load("data/yearly/yearly_training.rda")
+length_y_train <- yearly_training$N
+summary(length_y_train)
+length(length_y_train)
+## Quarterly
+qt <- load("data/quarterly/quarterly_training.rda")
+length_q_train <- quarterly_training$N
+summary(length_q_train)
+length(length_q_train)
+## Monthly
+mt <- load("data/monthly/monthly_training.rda")
+length_m_train <- monthly_training$N
+summary(length_m_train)
+length(length_m_train)
+## Daily
+dt <- load("data/daily/daily_training.rda")
+length_d_train <- daily_training$N
+summary(length_q_train)
+length(length_d_train)
+## Hourly
+ht <- load("data/hourly/hourly_training.rda")
+length_h_train <- hourly_training$N
+summary(length_h_train)
+length(length_h_train)
+
+
