ce8e780c1796bcb51cea478b50eb52764851374f
thiyangt <thiyanga.talagala@monash.edu>
2022-11-23 14:36:06 +0530

    added script file

--- src/review_comments_codes_11_2022.R
+++ src/review_comments_codes_11_2022.R
@@ -1,8 +1,6 @@
 ## Packages
 library(tidyverse)
 library(Mcomp)
-##install.packages("https://github.com/carlanetto/M4comp2018/releases/download/0.2.0/M4comp2018_0.2.0.tar.gz",
-##                repos=NULL)
 ##remotes::install_github("carlanetto/M4comp2018", ref = "6e75d59eb30e47cbb6bd5093d5bf2515493a6050")
 
 library(M4comp2018)
@@ -24,21 +22,7 @@ length_W <- data.frame(length=sapply(weekly_M4, function(temp){length(temp$x)}))
 length_D <- data.frame(length=sapply(daily_M4, function(temp){length(temp$x)}))
 length_H <- sapply(hourly_M4, function(temp){length(temp$x)})
 
-class(length_Y) # dataframe
-summary(length_Y[,1])
-length(length_Y[, 1])
 
-summary(length_Q[, 1])
-length(length_Q[, 1])
-
-summary(length_M[, 1])
-length(length_M[, 1])
-
-summary(length_D[,1])
-length(length_D[,1])
-
-summary(length_H[, 1])
-length(length_H[, 1])
 ## Training data length (M1+M3+Simulated)
 
 ## Yearly
@@ -46,25 +30,39 @@ yt <- load("data/yearly/yearly_training.rda")
 length_y_train <- yearly_training$N
 summary(length_y_train)
 length(length_y_train)
+sd(length_y_train)
+
 ## Quarterly
 qt <- load("data/quarterly/quarterly_training.rda")
 length_q_train <- quarterly_training$N
 summary(length_q_train)
 length(length_q_train)
+sd(length_q_train)
+
 ## Monthly
 mt <- load("data/monthly/monthly_training.rda")
 length_m_train <- monthly_training$N
 summary(length_m_train)
 length(length_m_train)
+sd(length_m_train)
+
+## Weekly
+wt <- load("data/weekly/weekly_training.rda")
+length_w_train <- weekly_training$N
+summary(length_w_train)
+length(length_w_train)
+sd(length_w_train)
+
 ## Daily
 dt <- load("data/daily/daily_training.rda")
 length_d_train <- daily_training$N
 summary(length_q_train)
 length(length_d_train)
+sd(length_d_train)
+
 ## Hourly
 ht <- load("data/hourly/hourly_training.rda")
 length_h_train <- hourly_training$N
 summary(length_h_train)
 length(length_h_train)
-
-
+sd(length_h_train)
